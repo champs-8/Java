4bdd6156d52f300af17b2c55690f73e895676d00
champs-8 <fabio.s.dev@outlook.com>
2025-02-22 18:39:57 -0300

    commit initial

---
+++ GerenciadorEstoque/src/model/Produto.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Produto {
+    //definir variaveis
+    private int id, quantity;
+    private String name;
+    private float price;
+
+
+    //contrutor
+    public Produto (int id, String name, float price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    };
+
+    //getters e setters
+    public int getId(){
+        return id; 
+    }
+    public void setId(int id){ //receberá um valor como parametro
+        this.id = id; //adicionará esse valor a varivel do construtor
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity (int quantity) {
+        this.quantity = quantity;
+    }
+
+    // @Override
+    //metodo para mostrar infoProdutos
+    public String infoProdString () {
+        return String.format("ID: %-4d - Nome: %-20s - Preco: - %-8.2f - Quantidade: %-5d", id, name, price, quantity);
+    }
+}
