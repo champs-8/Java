997ba6e7f48f5250247632288faf915545e98ad2
champs-8 <fabio.s.dev@outlook.com>
2025-03-18 20:54:33 -0300

    iniciando projeito Spring Boot

---
+++ UserApi/src/test/java/com/champs/UserApi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champs.UserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
