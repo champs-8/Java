9f7d60010752cbf16b498c14598ac1311b703329
champs-8 <fabio.s.dev@outlook.com>
2025-03-19 21:49:06 -0300

    Feito a entidade User com getters e setters

--- UserApi/src/main/java/com/champs/UserApi/UserApiApplication.java
+++ UserApi/src/main/java/com/champs/UserApi/UserApiApplication.java
@@ -8,6 +8,6 @@ public class UserApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
+		System.out.println("hello world");
 	}
-
 }
