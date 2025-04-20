299c37b54b51e10d8b11523e23d1f52a0cc5e661
champs-8 <fabio.s.dev@outlook.com>
2025-04-20 16:51:01 -0300

    adiciona implementação do serviço e controlador de times, incluindo métodos para criar, listar, atualizar e deletar

--- sprinboot2-essentials/src/main/java/com/champs/sprinboot2_essentials/controller/TeamController.java
+++ sprinboot2-essentials/src/main/java/com/champs/sprinboot2_essentials/controller/TeamController.java
@@ -2,29 +2,59 @@ package com.champs.sprinboot2_essentials.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.champs.sprinboot2_essentials.model.Team;
-import com.champs.sprinboot2_essentials.services.TeamServices;
+import com.champs.sprinboot2_essentials.request.TeamPostRequestBody;
+import com.champs.sprinboot2_essentials.request.TeamPutRequestBody;
+import com.champs.sprinboot2_essentials.services.TeamService;
 
+import lombok.Data;
+
+@Data
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
+    private final TeamService teamService;
 
-    @Autowired
-    private TeamServices teamServices;
-
-    @GetMapping //endpoint quando entrar na rota teams
-    public List<Team> lista() {
-        return teamServices.lista();
+    @GetMapping("/{id}") //endpoint quando entrar na rota teams/{id}
+    public ResponseEntity<Team> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(teamService.findById(id));
     }
-    @PostMapping("/create") //endpoint criar time
-    public Team createTeam(@RequestBody Team team){ //essa requisição será coletada no corpo
-        return teamServices.createTeam(team);
+
+    @GetMapping //endpoint quando entrar na rota teams para buscar todos os objetos
+    public List<Team> lista() {
+        return teamService.lista();
+    }
+
+    @PostMapping //endpoint para criar um novo objeto
+    public ResponseEntity<Team> save(@RequestBody TeamPostRequestBody teamPostRequestBody) {
+        return new ResponseEntity<>(
+            teamService.save(teamPostRequestBody), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}") //endpoint quando entrar na rota teams/{id} para deletar o objeto
+    //o id é passado como parametro
+    //o @PathVariable indica que o id é um parametro da url
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        teamService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}") // Endpoint para atualizar o objeto
+    public ResponseEntity<Void> replace(@PathVariable Long id,
+        @RequestBody TeamPutRequestBody teamPutRequestBody) {
+        teamPutRequestBody.setId(id); // Força o uso do id da URL
+        teamService.replace(teamPutRequestBody);
+        return ResponseEntity.noContent().build(); // Retorna 204 No Content
     }
 }
